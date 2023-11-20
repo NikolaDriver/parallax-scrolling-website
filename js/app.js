50e89773f139c5dbeb5d477d7bb11503de277683
Nikolay Tverdokhleb <nikolaytverdokhlebnew@gmail.com>
2023-11-20 20:32:11 +0200

    ewrw

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.addEventListener('scroll', e =>
-document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 )
+gsep.registerPlagin(ScrollTrigger, ScrolSmoother)
+ScrolSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
